88eae5835294cf4677986e0695aa252c4e345c46
Maarten <mmedaer@gmail.com>
2022-03-16 14:28:46 +0100

    Mine with no neighbours will open up "all around"

--- src/model/Minesweeper.java
+++ src/model/Minesweeper.java
@@ -114,12 +114,27 @@ public class Minesweeper extends AbstractMineSweeper {
     @Override
     public void open(int x, int y) {
         AbstractTile tempTile = getTile(x, y);
-        if (tempTile != null) {
-            tempTile.open();
-            this.viewNotifier.notifyOpened(x,y, explosiveNeighbourCount(x, y));
+        if (tempTile != null) { //If tile has explosive neighbours just open and nothing else
+            if (explosiveNeighbourCount(x,y) > 0) {
+                tempTile.open();
+                this.viewNotifier.notifyOpened(x,y, explosiveNeighbourCount(x, y));
+            }
+            else { //If tile has no explosive neighbours, open all tiles around it
+                tempTile.open();
+                this.viewNotifier.notifyOpened(x,y, explosiveNeighbourCount(x, y));
+
+                for(int i = -1; i <= 1; i++) {
+                    for(int j = -1; j <= 1; j++) {
+                        AbstractTile checkTile = getTile(x + i, y + j);
+                        if(checkTile != null && checkTile.isOpened() == false) {
+                            open(x + i, y + j);
+                        }
+                    }
+                }
+            }
         }
 
-        if (tempTile.isExplosive()) {
+        if (tempTile.isExplosive()) { //If tile is explosive, notify the view
             for (int tempX = 0; tempX < col; tempX++) {
                 for (int tempY = 0; tempY < row; tempY++) {
                     if (getTile(tempX, tempY).isExplosive()) {
